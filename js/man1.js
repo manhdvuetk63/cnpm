a567c7917ae7bc3f60f86df90f5fb9f4e74e6d1f
xuan123dthhbg <xuan123dthhbg@gmail.com>
2020-04-07 00:22:35 +0700

    cursor

--- js/man1.js
+++ js/man1.js
@@ -1,31 +1,21 @@
 $( function() {
     $( '.figure' ).draggable({ revert: "invalid" });
     $( "#orange_elephant" ).droppable({
-        accept: 'svg .tron',
+        accept: ' .tron',
         classes: {
             "ui-droppable-active": "ui-state-highlight",
             "ui-droppable-hover": "ui-state-hover"
-        },
-        drop: function( event, ui ) {
-            addtron();ui
         }
+
+
     });
 
     $( "#blue_elephant" ).droppable({
-        accept: 'svg .vuong',
+        accept: ' .vuong',
         classes: {
             "ui-droppable-active": "ui-state-active",
             "ui-droppable-hover": "ui-state-hover"
-        },
-        drop: function( event, ui ) {
-            $( this )
-                .addClass( "ui-state-highlight" )
-                .find( "p" )
-                .html( "Dropped!" );
         }
     });
-    fuction addtron($item){
-       
-    }
 } );
 
